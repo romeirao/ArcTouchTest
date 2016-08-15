d50df9a383334cf912ce5d16a3c712e7eabeef0d
Italo Romeiro Wanderley <romeirao@gmail.com>
2016-08-15 10:06:11 -0300

    One more test

--- src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchResultRobot.java
+++ src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchResultRobot.java
@@ -1,8 +1,11 @@
 package br.com.italoromeiro.arctouchtest.robots;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+
 import br.com.italoromeiro.arctouchtest.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -32,12 +35,18 @@ public class SearchResultRobot {
     public SearchResultRobot isSuccessWithoutResults() {
         onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
         onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
-        onView(withText(R.string.textview_label_route_empty)).check(matches(isDisplayed()));
+        onView(withText("No routes to display")).check(matches(isDisplayed()));
         return this;
     }
 
     public SearchResultRobot isFail() {
         onView(withText("We couldn\'t get info at this time. Please try again.")).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public SearchResultRobot reachDetailView() {
+        onView(withId(R.id.rv_routes)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.viewpager)).check(matches(isDisplayed()));
         return this;
     }
 }
