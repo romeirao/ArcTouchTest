d72fbc964b48a425b8932936aac21f329a5ae5c6
Italo Romeiro Wanderley <romeirao@gmail.com>
2016-08-15 09:16:46 -0300

    Adding more tests

---
+++ src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/core/TestConstants.java
@@ -0,0 +1,11 @@
+package br.com.italoromeiro.arctouchtest.core;
+
+/**
+ * Created by italo on 15/08/16.
+ */
+public class TestConstants {
+    public static final String STOP_NAME_GOOD_WITH_MANY_RESULTS = "stop_name_good_with_many_results";
+    public static final String STOP_NAME_GOOD_WITH_ONE_RESULT = "stop_name_good_with_one_result";
+    public static final String STOP_NAME_GOOD_WITHOUT_RESULTS = "stop_name_good_without_results";
+    public static final String STOP_NAME_FAIL = "stop_name_fail";
+}
--- src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchResultRobot.java
+++ src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchResultRobot.java
@@ -6,13 +6,38 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by italo on 13/08/16.
  */
 public class SearchResultRobot {
-    public SearchResultRobot isSuccess() {
+    public SearchResultRobot isSuccessWithManyResults() {
+        onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
+        onView(withText("2 routes found")).check(matches(isDisplayed()));
         onView(withId(R.id.rv_routes)).check(matches(isDisplayed()));
+        onView(withId(R.id.progress_container)).check(matches(not(isDisplayed())));
+        return this;
+    }
+
+    public SearchResultRobot isSuccessWithOneResult() {
+        onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
+        onView(withText("1 route found")).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_routes)).check(matches(isDisplayed()));
+        onView(withId(R.id.progress_container)).check(matches(not(isDisplayed())));
+        return this;
+    }
+
+    public SearchResultRobot isSuccessWithoutResults() {
+        onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_amount)).check(matches(isDisplayed()));
+        onView(withText(R.string.textview_label_route_empty)).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public SearchResultRobot isFail() {
+        onView(withText("We couldn\'t get info at this time. Please try again.")).check(matches(isDisplayed()));
         return this;
     }
 }
