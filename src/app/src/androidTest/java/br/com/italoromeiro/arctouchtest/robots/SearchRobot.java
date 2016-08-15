0d8d816ddc42bdf3301840ac143cce06edfb67c9
Italo Romeiro Wanderley <romeirao@gmail.com>
2016-08-15 06:11:48 -0300

    Minor fix in test

--- src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchRobot.java
+++ src/app/src/androidTest/java/br/com/italoromeiro/arctouchtest/robots/SearchRobot.java
@@ -4,21 +4,21 @@ import br.com.italoromeiro.arctouchtest.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by italo on 13/08/16.
  */
 public class SearchRobot {
     public SearchRobot where(String nameOfStreet) {
-        onView(withId(R.id.et_search)).perform(typeText(nameOfStreet));
+        onView(withId(R.id.et_search)).perform(typeText(nameOfStreet), closeSoftKeyboard());
         return this;
     }
 
     public SearchResultRobot search() {
-        onView(withText(R.string.button_label_search)).perform(click());
+        onView(withId(R.id.btn_search)).perform(click());
         return new SearchResultRobot();
     }
 }
